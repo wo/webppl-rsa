739bada3049670afbfd8efc93b002d8604383330
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2015-12-02 10:38:44 -0800

    Initial commit

---
+++ src/library.js
@@ -0,0 +1,10 @@
+// Javascript code goes here
+
+var exampleJavascriptFn = function(x) {
+  return x + 1;
+}
+
+module.exports = {
+  // Adjust exports here
+  exampleJavascriptFn: exampleJavascriptFn
+}
